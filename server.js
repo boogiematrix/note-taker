a192c1af6f4236ee8535a9b2fe8272ceee648def
Night Chough <chicken.snuggles@gmail.com>
2021-04-17 17:49:16 -0500

    linked db.json to api/notes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
+const database = require('./db/db.json')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,13 +9,12 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db/db.json'));
-
+    res.json(database)
 })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
