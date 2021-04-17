8546464a1299ec102f43374294158a17160735d2
Night Chough <chicken.snuggles@gmail.com>
2021-04-17 18:32:05 -0500

    linked css and js to html with express.static

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const database = require('./db/db.json')
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.use(express.static('public'))
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/', (req, res) => res.redirect('/index.html'));
+
+app.get('/notes', (req, res) => res.redirect('/notes.html'));
 
 app.get('/api/notes', (req, res) => {
     res.json(database)
 })
+
+app.post('/api/characters', (req, res) => {
+    const newNote = req.body;
+    database.push(newNote);
+    res.json(newNote);
+});
+
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
