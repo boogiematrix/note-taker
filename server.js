f1cf702e7801cc01b451e95ce3e08d7539f201f2
Night Chough <chicken.snuggles@gmail.com>
2021-04-17 18:57:57 -0500

    changed /notes.html filepath to /notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs')
 const database = require('./db/db.json')
-
+const uniqid = require('uniqid');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -13,17 +13,22 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.redirect('/index.html'));
 
-app.get('/notes', (req, res) => res.redirect('/notes.html'));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('/api/notes', (req, res) => {
     res.json(database)
 })
 
-app.post('/api/characters', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const newNote = req.body;
+    newNote.id = uniqid();
     database.push(newNote);
     res.json(newNote);
 });
+
+app.delete('/api/notes', (req, res) => {
+    
+})
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
